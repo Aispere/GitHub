c2eccf357b3e43dc78050667caf722e69baa797f
Lars Wendel <lwendel@student.tgm.ac.at>
2018-10-12 11:54:03 +0200

    first commit.

---
+++ src/charToInt.java
@@ -0,0 +1,10 @@
+
+public class charToInt {
+	public static void main(String[] args) {
+		int w = 'ä';
+		int x = 'ö';
+		int y = 'ü';
+		int z = 'ß';
+		System.out.println(w + " " + x + " " + y + " " + z);
+	}
+}
